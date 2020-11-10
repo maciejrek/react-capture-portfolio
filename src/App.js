8f31b5985de32c290cdd3282145001b312a1d896
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-10 13:13:45 +0100

    Add About Us section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <h1>film</h1>
+      <AboutUs />
     </div>
   );
 }
