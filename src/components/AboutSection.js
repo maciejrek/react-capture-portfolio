7a7a382ab4dac39bf1a9e816c06de6b48deee5b8
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-10 18:03:21 +0100

    Add styling to services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,7 +1,11 @@
 import React from "react";
 import home1 from "../img/home1.png";
-//Styled
-import styled from "styled-components";
+import {
+  StyledHide,
+  StyledAbout,
+  StyledDescription,
+  StyledImage,
+} from "../styles";
 
 const AboutSection = () => {
   return (
@@ -32,37 +36,5 @@ const AboutSection = () => {
     </StyledAbout>
   );
 };
-
-//Styled Components
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const StyledHide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
