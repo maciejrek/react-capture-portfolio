5b9781c2e7b2c77ad91e5e30303fecd1f32e4eff
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-11 14:18:57 +0100

    Page Transitions cleanup

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -11,41 +11,20 @@ import {
 import { motion } from "framer-motion";
 
 const AboutSection = () => {
-  const titleAnim = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { duration: 2 } },
-  };
-  const container = {
-    hidden: { x: 100 },
-    show: {
-      x: 0,
-      transition: {
-        duration: 0.75,
-        ease: "easeOut",
-        staggerChildren: 0.5,
-        when: "afterChildren",
-      },
-    },
-  };
   return (
     <StyledAbout>
       <StyledDescription>
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="title"
-        >
+        <motion.div>
           <StyledHide>
-            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+            <motion.h2>We work to make</motion.h2>
           </StyledHide>
           <StyledHide>
-            <motion.h2 variants={titleAnim}>
+            <motion.h2>
               your <span>dreams</span> come
             </motion.h2>
           </StyledHide>
           <StyledHide>
-            <motion.h2 variants={titleAnim}>true.</motion.h2>
+            <motion.h2>true.</motion.h2>
           </StyledHide>
         </motion.div>
         <p>
