1bfb8474edff63a24b0e740a26cf5841ff69d53c
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-11 13:22:31 +0100

    Add dynamic Movie Details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
+//Import Components
 import Nav from "./components/Nav";
 //Router
 import { Switch, Route } from "react-router-dom";
@@ -18,8 +20,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
