52dbf8ef2772694046fb5858706ae90dccb924d8
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-12 18:58:30 +0100

    Add scroll animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -4,10 +4,18 @@ import styled from "styled-components";
 import { StyledAbout } from "../styles";
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <StyledFaq>
+    <StyledFaq
+      variants={scrollReveal}
+      animate={controls}
+      initial="hidden"
+      ref={element}
+    >
       <h2>
         Any questions? <span>FAQ</span>
       </h2>
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,12 +2,12 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 const Toggle = ({ children, title }) => {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
   return (
     <motion.div layout className="question" onClick={() => setToggle(!toggle)}>
       <motion.h4 layout>{title}</motion.h4>
 
-      {toggle ? "" : children}
+      {toggle ? children : ""}
 
       <motion.div layout className="faq-line"></motion.div>
     </motion.div>
